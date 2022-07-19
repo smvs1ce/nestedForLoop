7265760dec8edc8118be55fb7ed33e4d41c9591a
John Hairston <j.hairston71@gmail.com>
2022-07-18 20:01:00 -0400

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+// count = 0;
+// window.onload = function () {
+//   document.getElementById("cbtn").addEventListener("click", function () {
+//     count += 1;
+//     document.getElementById(
+//       "h1"
+//     ).innerHTML = 'User has clicked on the button ${count} times!';
+//     console.log(count);
+//   });
+// };
